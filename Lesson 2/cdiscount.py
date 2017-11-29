eb185ba4c3a6434d9c8fc06b866ac92863670409
rmasliah <robin.masliah@telecom-paristech.fr>
2017-11-29 19:04:16 +0100

    update

--- Lesson 2/cdiscount.py
+++ Lesson 2/cdiscount.py
@@ -12,9 +12,19 @@ def getsearch(search):
         return None
 
 
-def getInfoFromOneProductSoup():
 
-    price
-PrintInfoForWholeSearchPage('acer')
-print('\n' + '=' * 50)
-PrintInfoForWholeSearchPage('dell')
+
+url1 = getsearch('acer')
+url2 = getsearch('dell')
+
+r = requests.get(url1)
+soup = BeautifulSoup(r.text, 'html.parser')
+machine = soup.tbody.find_all(class_= 'price')
+machine_raw = [m.text.split(' ', 1)[0] for m in machine]
+print(machine_raw)
+
+r = requests.get(url2)
+soup = BeautifulSoup(r.text, 'html.parser')
+machine = soup.tbody.find_all(class_= 'price')
+machine_raw = [m.text.split(' ', 1)[0] for m in machine]
+print(machine_raw)
