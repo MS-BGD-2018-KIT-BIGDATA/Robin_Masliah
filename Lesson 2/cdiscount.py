a529834d47ed2000e186f54fc359efc778447447
rmasliah <robin.masliah@telecom-paristech.fr>
2017-11-29 19:06:10 +0100

    update

--- Lesson 2/cdiscount.py
+++ Lesson 2/cdiscount.py
@@ -23,8 +23,24 @@ machine = soup.tbody.find_all(class_= 'price')
 machine_raw = [m.text.split(' ', 1)[0] for m in machine]
 print(machine_raw)
 
+r = requests.get(url1)
+soup = BeautifulSoup(r.text, 'html.parser')
+machine = soup.tbody.find_all(class_= 'prdtPrSt')
+machine_raw = [m.text.split(' ', 1)[0] for m in machine]
+print(machine_raw)
+
 r = requests.get(url2)
 soup = BeautifulSoup(r.text, 'html.parser')
 machine = soup.tbody.find_all(class_= 'price')
 machine_raw = [m.text.split(' ', 1)[0] for m in machine]
 print(machine_raw)
+
+r = requests.get(url2)
+soup = BeautifulSoup(r.text, 'html.parser')
+machine = soup.tbody.find_all(class_= 'prdtPrSt')
+machine_raw = [m.text.split(' ', 1)[0] for m in machine]
+print(machine_raw)
+
+print("Marque = ")
+print("Prix = ")
+print("Solde = ")
